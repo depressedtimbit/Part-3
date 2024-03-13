0364017d17db814acf29263978aefa02286c72f0
depressedtimbit <84045706+depressedtimbit@users.noreply.github.com>
2024-03-13 14:08:51 -0400

    completed task 1: added Archer

---
+++ Assets/Week 9/Scripts/Archer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Villager
+{
+    public GameObject arrowPrefab;
+    public Transform spawnPoint;
+
+    protected override void Attack()
+    {
+        base.Attack();
+        destination = transform.position;
+        Instantiate(arrowPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
