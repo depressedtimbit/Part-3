d2cf1969084d6680a6debfff85ad7a5a02919de7
depressedtimbit <84045706+depressedtimbit@users.noreply.github.com>
2024-03-20 13:59:08 -0400

    finished task 1

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TMPro.TextMeshProUGUI currentSelection;
+    public static CharacterControl Instance;
     public static Villager SelectedVillager { get; private set; }
+    public void Start() 
+    {
+        Instance = this;
+    }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -14,6 +21,7 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        Instance.currentSelection.text = SelectedVillager.ToString();
     }
     
 }
--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -1,24 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Thief : Villager
 {
     public GameObject knifePrefab;
-    public Transform[] spawnPoints;
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+    public float DashSpeed = 7;
+    
     
     protected override void Attack()
     {
-        base.Attack();
-        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = destination;
-        foreach (Transform point in spawnPoints)
-        {
-            Instantiate(knifePrefab, point.position, point.rotation);
-        }
+        StartCoroutine(Dash());
     }
+
+    IEnumerator Dash()
+    {
+        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        speed = DashSpeed;
+
+        while (speed > 3)
+        {
+            yield return null;
+        }
+        base.Attack();
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(knifePrefab, spawnPoint1.position, spawnPoint1.rotation);
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(knifePrefab, spawnPoint2.position, spawnPoint2.rotation);
+        
+        
+    }
+
+    
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
+    }
+
+    public override string ToString()
+    {
+        return "Hi I'm Bob";
     }
 }
